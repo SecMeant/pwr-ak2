83071f7a274fcf8d687cf64458e6d2d1fa2f6a76
secmeant <secmeant@gmail.com>
2019-04-26 12:21:33 +0200

    Removing printing from parsing div test now reads bignums from file

--- src/main.c
+++ src/main.c
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include "bignum_common.h"
 #include "bcd_common.h"
+#include "parse.h"
+
 void shift_test()
 {
   int64_t num1[] = {0,0,3,0};
@@ -145,20 +147,28 @@ void isPrime(){
 
 }
 
-bignum_divide_result div_test()
+void div_test()
 {
-  int64_t num1[8] = {0xffffffffffffffff, 0xffffffffffffffff,
-                     0xffffffffffffffff, 0xffffffffffffffff,
-                     0xffffffffffffffff, 0xffffffffffffffff,
-                     0xffffffffffffffff, 0xff};
-  int64_t num2[8] = {0xffffffffffffffff, 0xffffffffffffffff,
-                     0xffffffffffffffff, 0xffffffffffffffff,
-                     0xffffffffffffffff, 0xffffffffffffffff,
-                     0x7fffffffffffffff, 0x0};
+  FILE *f = fopen("bignums.txt", "rb");
 
-  bignum b1 = {.bignum = num1, .bignum_size = 8};
-  bignum b2 = {.bignum = num2, .bignum_size = 8};
-  return bignum_divide(b1,b2);
+  if(!f)
+  {
+    fprintf(stderr, "Could not open bignums.txt file.");
+    return;
+  }
+
+  bignum b1 = bignum_load(f);
+  bignum b2 = bignum_load(f);
+  bignum_print(b1);
+  bignum_print(b2);
+  bignum_divide_result res = bignum_divide(b1,b2);
+  bignum_print(res.result);
+  bignum_print(res.reminder);
+  bignum_free(res.reminder);
+  bignum_free(res.result);
+  bignum_free(b1);
+  bignum_free(b2);
+  fclose(f);
 }
 
 
@@ -329,6 +339,5 @@ void bcd_isPrime(){
 
 int main()
 {
-  power_test();
   return 0;
 }
